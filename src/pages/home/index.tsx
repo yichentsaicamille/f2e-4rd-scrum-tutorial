7a46648edfd30573da41d702cef21082e0105067
Camille_Tsai/WHQ/Wistron <Camille_Tsai@wistron.com>
2023-04-07 11:10:49 +0800

    home/index.tsx - 切版首頁

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,27 @@
 import * as Components from "components";
+import styles from "./styles.module.scss";
+import index from "assets/picture/jpg/index.jpg";
+import { ReactComponent as Title } from "assets/icon/svg/title.svg";
 
 const Home = () => {
-  return <></>;
+  const onClickLearnButton = () => {};
+
+  return (
+    <>
+      <div className={styles.container}>
+        <img src={index} className={styles.background} />
+        <Title className={styles.titleIcon} />
+        <Components.Button
+          className={styles.learnButton}
+          color={"#ACD271"}
+          width={"350px"}
+          onClick={onClickLearnButton}
+        >
+          <span className={styles.font}>進入學習</span>
+        </Components.Button>
+      </div>
+    </>
+  );
 };
 
 export default Home;

370028bde874d5e9992bfbb1f095af02f3c70bc5
Camille_Tsai/WHQ/Wistron <Camille_Tsai@wistron.com>
2023-04-07 17:03:40 +0800

    切版貓咪、植物、對話框位置

--- src/pages/introduce/index.tsx
+++ src/pages/introduce/index.tsx
@@ -1,8 +1,22 @@
 import * as Components from "components";
+import styles from "./styles.module.scss";
+import plant1 from "assets/picture/png/plant-1.png";
+import plant2 from "assets/picture/png/plant-2.png";
+import catAll from "assets/picture/png/cat-all.png";
+
 const Introduce = () => {
   return (
     <>
-      <Components.LayOut>{""}</Components.LayOut>
+      <Components.LayOut>
+        <div className={styles.content}>
+          哈囉~歡迎進入「SCRUM 新手村」 <br />
+          在正式加入專案開發之前，需要請你先了解 Scrum 的流程與精神！
+          請接受挑戰任務，成功通過 Scrum 新手村的挑戰任務吧～～
+        </div>
+        <img src={plant1} className={styles.plant1} />
+        <img src={catAll} className={styles.allCats} />
+        <img src={plant2} className={styles.plant2} />
+      </Components.LayOut>
     </>
   );
 };
